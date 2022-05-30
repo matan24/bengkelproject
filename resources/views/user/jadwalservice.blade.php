521de4c890cfc23dd62b58bd6cba03ef933c9ab2
matan24 <ferrypamatan0486@gmail.com>
2022-05-30 23:50:08 +0700

    eh ga jadi tamat

--- resources/views/user/jadwalservice.blade.php
+++ resources/views/user/jadwalservice.blade.php
@@ -83,7 +83,7 @@
                   <li><a href="#">Home</a></li>
                   <li><a href="#">Jadwal Service</a></li>
                   <li><a href="#">Service Sekarang</a></li>
-                  <li><a href="#">Contact</a></li>
+                  <li><a href="#">Contacts</a></li>
                 </ul>
               </div>
     
